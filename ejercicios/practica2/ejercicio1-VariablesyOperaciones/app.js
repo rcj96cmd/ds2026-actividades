38b8ec7b842aec6c956c5a8505cfe2c47d416011
rcj96cmd <rodrigocariaga128@gmail>
2026-04-14 15:16:07 -0300

    Ejercicio 1 - Variables y operaciones

---
+++ ejercicios/practica2/ejercicio1-VariablesyOperaciones/app.js
@@ -0,0 +1,26 @@
+//Variables
+const nombre = 'Rodrigo';
+const edad = '29';
+const materia = 'Desarrollo de Software';
+
+//Mensaje Template Literals
+const mensaje = `Hola ${nombre}, tengo ${edad} años y curso ${materia}`;
+
+console.log(mensaje);
+
+//Contador
+
+let contador = 0;
+
+//Sumar 1 tres veces
+
+for (let i = 0; i < 3; i++){
+
+    contador++;
+
+}
+
+//Mostrar resultado final
+
+console.log("Valor final del contador:", contador);
+
