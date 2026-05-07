2a2f76b3d200fe6d433c6c3cedc146c1b7e362dc
Cariaga Rodrigo Jonathan <rodrigocariaga.128@gmail.com>
2026-05-07 11:01:30 -0300

    Ejercicio 3 - Buscador de Libros

---
+++ ejercicios/c05-async-await+fetch/ejercicio3-BuscadordeLibros/app.js
@@ -0,0 +1,52 @@
+"use strict";
+const inputBusqueda = document.getElementById("busqueda");
+const btnBuscar = document.getElementById("btnBuscar");
+const resultados = document.getElementById("resultados");
+const mensajeError = document.getElementById("mensajeError");
+//Evento click
+btnBuscar.addEventListener("click", buscarLibros);
+//Función principal
+async function buscarLibros() {
+    resultados.innerHTML = "";
+    mensajeError.textContent = "";
+    const textoBusqueda = inputBusqueda.value.trim();
+    if (textoBusqueda === "") {
+        mensajeError.textContent = "Ingrese un libro para buscar.";
+        return;
+    }
+    try {
+        const respuesta = await fetch(`https://openlibrary.org/search.json?q=${textoBusqueda}`);
+        if (!respuesta.ok) {
+            throw new Error("Error en la búsqueda");
+        }
+        const datos = await respuesta.json();
+        mostrarResultados(datos.docs);
+    }
+    catch (error) {
+        mensajeError.textContent = "Ocurrió un error al buscar libros.";
+        console.error(error);
+    }
+}
+//Resultados
+function mostrarResultados(libros) {
+    libros.slice(0, 10).forEach(libro => {
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "tarjeta";
+        const autor = libro.author_name
+            ? libro.author_name[0]
+            : "Autor desconocido";
+        const anio = libro.first_publish_year
+            ? libro.first_publish_year
+            : "Sin año";
+        tarjeta.innerHTML = `
+    
+      <h3>${libro.title}</h3>
+
+      <p><strong>Autor:</strong> ${autor}</p>
+
+      <p><strong>Año:</strong> ${anio}</p>
+
+    `;
+        resultados.appendChild(tarjeta);
+    });
+}
